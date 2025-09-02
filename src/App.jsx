2458908dfbc38d668af152ecce1dd7a9c24d5082
Owen Prasimsha <owenprasimsha84@gmail.com>
2025-09-02 20:38:09 +0700

    Chore: first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="p-6 text-center">
+        <h1 className="text-2xl font-bold text-blue-600">
+          Hello Vite + React + Tailwind 🚀
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
