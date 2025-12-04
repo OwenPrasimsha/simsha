20320db21ccd4b8927a4804d4e7e34499593cf7c
Owen Prasimsha <owenprasimsha84@gmail.com>
2025-12-05 00:26:14 +0700

    fix : typo number

--- src/components/common/HeroContent.jsx
+++ src/components/common/HeroContent.jsx
@@ -10,7 +10,7 @@ export default function HeroContent() {
       <div className="flex felx-wrap gap-4 mb-6">
         <ButtonLink
           variant="primary"
-          href="https://wa.me/62812274183327"
+          href="https://wa.me/6281227418327"
           target="_blank"
         >
           Get in Touch
